31a26d25729dbbc6ded88ace68f5d7f301da37b7
Deedo67 <70287389+Deedo67@users.noreply.github.com>
2021-06-06 22:47:23 -0400

    Commit math

--- src/main/java/MPE13/example/ex13.java
+++ src/main/java/MPE13/example/ex13.java
@@ -15,7 +15,23 @@ public class ex13 {
         Scanner in = new Scanner(System.in);
 
         //input
-        System.out.print("Enter the principal: ");
+        System.out.print("What is the principal amount? ");
         String strprin = in.nextLine();
+        System.out.print("What is the rate? ");
+        String strrate = in.nextLine();
+        System.out.print("What is the number of years? ");
+        String stryears = in.nextLine();
+        System.out.print("What is the number of times the interest is compounded per year? ");
+        String strcom = in.nextLine();
+
+        //string convert
+        int prin = Integer.parseInt(strprin);
+        double rate = Double.parseDouble(strrate);
+        int years = Integer.parseInt(stryears);
+
+        //math
+        double finrate = rate * 0.01;
+        double total = prin * (1 + (finrate * years));
+
     }
 }
