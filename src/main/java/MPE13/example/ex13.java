8955a14e4453122d26a2e3cee047695b64812839
Deedo67 <70287389+Deedo67@users.noreply.github.com>
2021-06-06 22:44:47 -0400

    Initial commit

---
+++ src/main/java/MPE13/example/ex13.java
@@ -0,0 +1,21 @@
+package MPE13.example;
+
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Derek Gilberti
+ */
+
+//class begin
+public class ex13 {
+    //main method begin
+    public static void main(String[] args) {
+        //scanner
+        Scanner in = new Scanner(System.in);
+
+        //input
+        System.out.print("Enter the principal: ");
+        String strprin = in.nextLine();
+    }
+}
